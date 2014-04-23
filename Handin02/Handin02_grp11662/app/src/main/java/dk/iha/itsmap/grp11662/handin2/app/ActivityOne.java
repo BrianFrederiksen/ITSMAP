3158545b49426b3e34a87589eedace752f08a87a
Shiphter <stephantl@gmail.com>
2014-04-23 14:33:15 +0200

    Initial commit of android project for handin02

---
+++ Handin02/Handin02_grp11662/app/src/main/java/dk/iha/itsmap/grp11662/handin2/app/ActivityOne.java
@@ -0,0 +1,15 @@
+package dk.iha.itsmap.grp11662.handin2.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ActivityOne extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+    }
+}
