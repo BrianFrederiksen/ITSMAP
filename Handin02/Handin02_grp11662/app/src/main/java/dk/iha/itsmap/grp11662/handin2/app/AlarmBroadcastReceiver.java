afa8a2a5465d99605997d91769d43138b7e3c6cc
Brian Frederiksen <Fessor@gmail.com>
2014-04-24 15:12:10 +0200

    added AlarmBroadcastReceiver

---
+++ Handin02/Handin02_grp11662/app/src/main/java/dk/iha/itsmap/grp11662/handin2/app/AlarmBroadcastReceiver.java
@@ -0,0 +1,17 @@
+package dk.iha.itsmap.grp11662.handin2.app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmBroadcastReceiver extends BroadcastReceiver {
+    public AlarmBroadcastReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
