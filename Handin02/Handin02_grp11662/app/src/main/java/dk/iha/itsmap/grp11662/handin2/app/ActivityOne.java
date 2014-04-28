d890f943a9f403e06c5108eabfe43b0dbda15b38
Shiphter <stephantl@gmail.com>
2014-04-28 11:19:22 +0200

    ActivityOne now starts servicealarm and disables GUI.  Has BroadcastReceiver to be called by alarm.

--- Handin02/Handin02_grp11662/app/src/main/java/dk/iha/itsmap/grp11662/handin2/app/ActivityOne.java
+++ Handin02/Handin02_grp11662/app/src/main/java/dk/iha/itsmap/grp11662/handin2/app/ActivityOne.java
@@ -1,7 +1,10 @@
 package dk.iha.itsmap.grp11662.handin2.app;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -14,6 +17,15 @@ import android.widget.EditText;
 public class ActivityOne extends Activity {
     private final String STATE_TAG = "State changed ";
     private final String BUTTON_TAG = "Button ";
+    public static final String COUNTDOWN_NOTIFICATION = "dk.iha.itsmap.grp11662.handin2.app.ActivityOne.CountdownNotification";
+
+    BroadcastReceiver countdownReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            EditText editText = (EditText) ActivityOne.this.findViewById(R.id.editText);
+            editText.setText((CharSequence) intent.getExtras().get("countdown"));
+        }
+    };
 
 
     @Override
@@ -21,10 +33,27 @@ public class ActivityOne extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
         setupButton((Button) findViewById(R.id.button),(EditText) findViewById(R.id.editText));
+
         Log.i(STATE_TAG, "onCreate");
     }
 
-    private void setupButton(Button button, final EditText editText) {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(STATE_TAG, "onResume");
+
+        registerReceiver(countdownReceiver,new IntentFilter(COUNTDOWN_NOTIFICATION));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(STATE_TAG,"onPause");
+
+        unregisterReceiver(countdownReceiver);
+    }
+
+    private void setupButton(final Button button, final EditText editText) {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,6 +63,11 @@ public class ActivityOne extends Activity {
                 serviceIntent.putExtra("alarmtime", editText.getText());
                 serviceIntent.putExtra("notification","Notification data from ActivityOne");
                 startService(serviceIntent);
+
+                //Disable UI interaction
+                button.setClickable(false);
+                button.setActivated(false);
+                editText.setFocusable(false);
             }
         });
     }
