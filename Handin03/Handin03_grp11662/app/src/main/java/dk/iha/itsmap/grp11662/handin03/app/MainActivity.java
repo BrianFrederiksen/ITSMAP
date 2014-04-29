41c310ffd0e9fc43c850ca0a6019d12a4383c014
Shiphter <stephantl@gmail.com>
2014-04-29 11:45:32 +0200

    changed fragment to support fragment

--- Handin03/Handin03_grp11662/app/src/main/java/dk/iha/itsmap/grp11662/handin03/app/MainActivity.java
+++ Handin03/Handin03_grp11662/app/src/main/java/dk/iha/itsmap/grp11662/handin03/app/MainActivity.java
@@ -2,8 +2,6 @@ package dk.iha.itsmap.grp11662.handin03.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
--- Handin03/Handin03_grp11662/app/src/main/java/dk/iha/itsmap/grp11662/handin03/app/MenuFragment.java
+++ Handin03/Handin03_grp11662/app/src/main/java/dk/iha/itsmap/grp11662/handin03/app/MenuFragment.java
@@ -1,6 +1,6 @@
 package dk.iha.itsmap.grp11662.handin03.app;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
