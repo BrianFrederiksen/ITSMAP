89003cc47cd677b10712c37de06dd57345557d6c
Shiphter <stephantl@gmail.com>
2014-04-29 11:40:25 +0200

    Added fragment, and included it in MainActivity

---
+++ Handin03/Handin03_grp11662/app/src/main/java/dk/iha/itsmap/grp11662/handin03/app/MenuFragment.java
@@ -0,0 +1,16 @@
+package dk.iha.itsmap.grp11662.handin03.app;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MenuFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_menu, container, false);
+    }
+}
