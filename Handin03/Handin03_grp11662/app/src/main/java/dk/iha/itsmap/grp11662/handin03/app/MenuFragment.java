457376222ded281cfd4c5813de6a1b4b09c3a0d8
Shiphter <stephantl@gmail.com>
2014-05-08 12:32:08 +0200

    Furhter work on MainActivity and MainFragment.

--- Handin03/Handin03_grp11662/app/src/main/java/dk/iha/itsmap/grp11662/handin03/app/MenuFragment.java
+++ Handin03/Handin03_grp11662/app/src/main/java/dk/iha/itsmap/grp11662/handin03/app/MenuFragment.java
@@ -5,12 +5,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 
 public class MenuFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        ListView choiceMenuList = (ListView) container.findViewById(R.id.choice_menu_list);
+
         return inflater.inflate(R.layout.fragment_menu, container, false);
     }
 }
