8faa31f5f120d1d3e01ea402178268f8ba3b7432
Shiphter <stephantl@gmail.com>
2014-05-13 12:05:22 +0200

    Search functionality works, and TrainService is started when button is pressed.

--- Handin04/Handin04_grp11662/app/src/main/java/dk/iha/itsmap/grp11662/handin04/app/MainActivity.java
+++ Handin04/Handin04_grp11662/app/src/main/java/dk/iha/itsmap/grp11662/handin04/app/MainActivity.java
@@ -1,17 +1,69 @@
 package dk.iha.itsmap.grp11662.handin04.app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ListView listView;
+    private EditText inputSearch;
+    private Button updateButton;
+    ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Listview Data
+        String trains[] = {"Dell Inspiron", "HTC One X", "HTC Wildfire S", "HTC Sense", "HTC Sensation XE",
+                "iPhone 4S", "Samsung Galaxy Note 800",
+                "Samsung Galaxy S3", "MacBook Air", "Mac Mini", "MacBook Pro"};
+
+        listView = (ListView) findViewById(R.id.listView);
+        inputSearch = (EditText) findViewById(R.id.inputSearch);
+        updateButton = (Button) findViewById(R.id.update_button);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, trains);
+        listView.setAdapter(adapter);
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(new Intent(v.getContext(), TrainService.class));
+            }
+        });
+
+        inputSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                MainActivity.this.adapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
 
 
