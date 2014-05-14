19fe66396d0e5ff39eba0d494b6e0086ebfa928e
11662 <mulatjavsen@hotmail.com>
2014-05-14 11:42:40 +0200

    added

---
+++ Handin04/Handin04_grp11662/app/src/main/java/dk/iha/itsmap/grp11662/handin04/app/Train.java
@@ -0,0 +1,44 @@
+package dk.iha.itsmap.grp11662.handin04.app;
+
+import java.lang.String; /**
+ * Created by shiphter on 30/04/14.
+ */
+public class Train {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWid() {
+        return wid;
+    }
+
+    public void setWid(String wid) {
+        this.wid = wid;
+    }
+
+    public String getX() {
+        return x;
+    }
+
+    public void setX(String x) {
+        this.x = x;
+    }
+
+    public String getY() {
+        return y;
+    }
+
+    public void setY(String y) {
+        this.y = y;
+    }
+
+    private String name;
+    private String wid;
+    private String x;
+    private String y;
+}
