bdab6213955d71b175695ec0484e5e1f530f8c2d
11662 <mulatjavsen@hotmail.com>
2014-05-15 13:18:59 +0200

    Implemented TrainServiceBinder

--- Handin04/Handin04_grp11662/app/src/main/java/dk/iha/itsmap/grp11662/handin04/app/TrainService.java
+++ Handin04/Handin04_grp11662/app/src/main/java/dk/iha/itsmap/grp11662/handin04/app/TrainService.java
@@ -2,11 +2,14 @@ package dk.iha.itsmap.grp11662.handin04.app;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
 public class TrainService extends Service {
+
+    private final IBinder myBinder = new TrainServiceBinder();
 
     public TrainService() {
 
@@ -16,8 +19,7 @@ public class TrainService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i("State changed", "TrainService: onStartCommand()");
         Toast.makeText(getApplicationContext(), "TOASTY", Toast.LENGTH_LONG).show();
-
-        //RestAccess restAccess = new RestAccess(this);
+        //RestAccess restAccess = new RestAccess(intent.getAction());
         //restAccess.getTrainStations().execute("http://stog.itog.dk/itog/action/list/format/json");
 
         return super.onStartCommand(intent, flags, startId);
@@ -26,6 +28,13 @@ public class TrainService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+
+        return myBinder;
+    }
+
+    public class TrainServiceBinder extends Binder {
+        TrainService getService() {
+            return TrainService.this;
+        }
     }
 }
